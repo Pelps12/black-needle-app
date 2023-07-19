7daee156853f46c920d92c8d5a1f3c0cd6b52580
Pelps12 <oluwapelps@gmail.com>
2023-07-19 01:54:16 -0500

    Fix chat bug (3)

--- apps/nextjs/src/pages/api/ably/webhook.ts
+++ apps/nextjs/src/pages/api/ably/webhook.ts
@@ -45,7 +45,7 @@ export default async function ably(req: NextApiRequest, res: NextApiResponse) {
 										id: roomId
 									}
 								},
-								type: data.extras.headers?.type ?? 'TEXT',
+								type: data.extras?.headers?.type ?? 'TEXT',
 								user: {
 									connect: {
 										id: data.clientId
