af9c03095fb65bdc4c282e0a6b40bec6a3179ec4
Pelps12 <oluwapelps@gmail.com>
2023-08-07 22:37:28 -0500

    Standardize push notification token

--- apps/expo/src/components/OAuth/Wrapper.tsx
+++ apps/expo/src/components/OAuth/Wrapper.tsx
@@ -1,15 +1,49 @@
 import React from "react";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
 import Constants from "expo-constants";
 import * as Device from "expo-device";
 import { createURL } from "expo-linking";
-import { getExpoPushTokenAsync } from "expo-notifications";
+import * as Notifications from "expo-notifications";
 import { useRouter } from "expo-router";
 import { useOAuth } from "@clerk/clerk-expo";
 import { trpc } from "@utils/trpc";
 
 import AppleButton from "./AppleButton";
 import GoogleButton from "./GoogleButton";
+
+async function registerForPushNotificationsAsync() {
+  let token: Notifications.ExpoPushToken | undefined;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig?.extra?.eas.projectId,
+    });
+    console.log(token);
+  } else {
+    token = undefined;
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
 
 const OAuthWrapper = ({ mode }: { mode: "signin" | "signup" }) => {
   const router = useRouter();
@@ -18,6 +52,7 @@ const OAuthWrapper = ({ mode }: { mode: "signin" | "signup" }) => {
     strategy: "oauth_google",
   });
   const { startOAuthFlow: appleFlow } = useOAuth({ strategy: "oauth_apple" });
+
   const handleSignInWithGooglePress = React.useCallback(async () => {
     try {
       const { createdSessionId, setActive } = await startOAuthFlow({
@@ -25,10 +60,8 @@ const OAuthWrapper = ({ mode }: { mode: "signin" | "signup" }) => {
       });
       if (createdSessionId && setActive) {
         setActive({ session: createdSessionId });
-        if (Device.isDevice) {
-          const token = await getExpoPushTokenAsync({
-            projectId: Constants.expoConfig?.extra?.eas?.projectId,
-          });
+        const token = await registerForPushNotificationsAsync();
+        if (token) {
           tokenMutation.mutate({
             expoToken: token.data,
           });
